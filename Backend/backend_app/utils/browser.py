01461a0fba2c700769ed8a5549372195657ae773
ogle <132525601+oglenopro@users.noreply.github.com>
2026-03-05 16:28:57 +0900

    Добавлена поддержка pywebview

--- Backend/backend_app/utils/browser.py
+++ Backend/backend_app/utils/browser.py
@@ -1,44 +1,6 @@
-import os
-import shutil
-import subprocess
-
-
+import webview
+    
 def open_in_browser(url):
-    if "DISPLAY" not in os.environ:
-        print("[WARN] No DISPLAY found, GUI not available")
-        return False
-
-    browsers = [
-        (
-            "chromium-browser",
-            [
-                "--disable-infobars",
-                "--noerrdialogs",
-                "--window-focus",
-                "--start-fullscreen",
-                "--password-store=basic",
-                "--user-data-dir=/tmp/chrome_temp_profile",
-            ],
-        ),
-        (
-            "chromium",
-            [
-                "--disable-infobars",
-                "--noerrdialogs",
-                "--window-focus",
-                "--start-fullscreen",
-                "--password-store=basic",
-                "--user-data-dir=/tmp/chrome_temp_profile",
-            ],
-        ),
-    ]
-
-    for browser, args in browsers:
-        path = shutil.which(browser)
-        if path:
-            subprocess.Popen([path, *args, url], env=os.environ)
-            print(f"[INFO] Opened in {browser}")
-            return True
-
-    print("[WARN] Chromium not found")
+    webview.create_window("Gesturesphone", url, resizable=True)
+    webview.start()
     return False
